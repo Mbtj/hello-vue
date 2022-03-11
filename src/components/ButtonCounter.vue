d59171272bf07f0a1cd6c0ae1bbb739a5daf4d1f
TJ <michael.btj@hotmail.com>
2022-03-10 20:46:55 -0500

    fix: create data for counter

--- src/components/ButtonCounter.vue
+++ src/components/ButtonCounter.vue
@@ -1,13 +1,25 @@
 <template>
   <h2>Counter: {{ count }}</h2>
-  <button @click="count++">Increment</button> 
+  <button @click="increment">Increment</button>
 </template>
 
 <script>
 export default {
   name: "ButtonCounter",
   props: {
-    count: Number
-  }
-}
+    initialCount: Number,
+  },
+
+  data() {
+    return {
+      count: this.initialCount,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
 </script>
