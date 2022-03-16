17d0a90513bb8d895975676f2cc1e5363c0e311f
TJ <michael.btj@hotmail.com>
2022-03-15 22:51:03 -0400

    edit style

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,7 @@ export default {
 </script>
 
 <style>
-#app,
-#app-extended {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
